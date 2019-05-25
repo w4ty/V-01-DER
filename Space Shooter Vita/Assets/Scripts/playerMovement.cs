9d60ae6186c5a1b9bb7e4f7e9956b7834a2b040b
w4ty <therex102@gmail.com>
2019-05-25 15:09:43 +0200

    Rotation and movement script

---
+++ Space Shooter Vita/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+	float maxSpeed = 5f;
+	float rotSpeed = 180f;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+		// Rotation
+		Quaternion rot = transform.rotation;
+		float z = rot.eulerAngles.z;
+		z -= Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
+		rot = Quaternion.Euler(0, 0, z);
+		transform.rotation = rot;
+		// Vertical movement
+		Vector3 pos = transform.position;
+
+		pos.y += Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime;
+
+		transform.position = pos;
+
+	}
+}
