6a21c126136c606db767fc26ab8423550f84db18
w4ty <therex102@gmail.com>
2019-05-29 23:08:12 +0200

    Collisions and damage handler (first part)

---
+++ Space Shooter Vita/Assets/Scripts/damageHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class damageHandler : MonoBehaviour
+{
+	public int maxHP = 2;
+	public int hp;
+	float invFrames = 0;
+	void Start()
+	{
+		hp = maxHP;
+	}
+	void OnTriggerEnter2D()
+	{
+		Debug.Log("Triggered");
+		if(invFrames <= 0)
+		{
+			hp -= maxHP / 2;
+			invFrames = 2f;
+		}
+	}
+	void Update()
+	{
+		if (hp <= 0)
+		{
+			kill();
+		}
+	}
+	void kill()
+	{
+		Destroy(gameObject);
+	}
+
+}
