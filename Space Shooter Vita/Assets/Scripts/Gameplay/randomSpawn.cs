a910fc36d0bbfc00426c66a36824abf26365a963
w4ty <therex102@gmail.com>
2019-06-16 00:01:36 +0200

    Basic Main Menu, logo, fixes to aspect ratio scaling

--- Space Shooter Vita/Assets/Scripts/Gameplay/randomSpawn.cs
+++ Space Shooter Vita/Assets/Scripts/Gameplay/randomSpawn.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class randomSpawn : MonoBehaviour {
 	public GameObject UnitToSpawn;
+	public GameObject UnitToSpawn2;
+	public GameObject UnitToSpawn3;
 	public GameObject Portal;
 	public float spawnCD;
+	public float spawnCD2;
+	public float spawnCD3;
 
-	
+
 	void Update () {
 		spawnCD -= 1;
 		if (spawnCD <= 0)
@@ -15,5 +19,18 @@ public class randomSpawn : MonoBehaviour {
 			spawnCD = 180f;
 			Instantiate(UnitToSpawn, Portal.transform.position, transform.rotation);
 		}
+		spawnCD2 -= 1;
+		if (spawnCD2 <= 0)
+		{
+			spawnCD2 = 240f;
+			Instantiate(UnitToSpawn2, Portal.transform.position, transform.rotation);
+		}
+		/* spawnCD3 -= 1;
+		if (spawnCD3 <= 0)
+		{
+			spawnCD3 = 180f;
+			Instantiate(UnitToSpawn3, Portal.transform.position, transform.rotation);
+		}
+		*/
 	}
 }
